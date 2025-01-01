625f1aa4096546604cf214fb9ed0c3b093fe90a4
Ahukko8 <ahukko88@gmail.com>
2025-01-01 14:44:28 +0500

    hero section and bento grid done

--- data/index.ts
+++ data/index.ts
@@ -29,7 +29,7 @@ export const navItems = [
     {
       id: 3,
       title: "Our tech stack",
-      description: "We constantly improve and update with modern and latest technologies",
+      description: "We constantly adapt to modern technologies",
       className: "lg:col-span-2 md:col-span-3 md:row-span-2",
       imgClassName: "",
       titleClassName: "justify-center",
@@ -49,7 +49,7 @@ export const navItems = [
   
     {
       id: 5,
-      title: "Currently building a JS Animation library",
+      title: "Currently looking for businesses to  grow with",
       description: "The Inside Scoop",
       className: "md:col-span-3 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Add your configuration options here
+};
+
+module.exports = nextConfig;
